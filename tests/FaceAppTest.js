1a58ade19dca3644c6592e766497ba56331a678c
emilyyoungss1 <emilyyoungss@hotmail.co.uk>
2020-06-18 22:25:24 +0900

    nightwatch testing
    Added tests with nightwatch for ensuring correct formatting of images, both before and after the ajax calls. Further tests for ajax calls to ensure that 4 images are returned in each shuffle.

---
+++ tests/FaceAppTest.js
@@ -0,0 +1,28 @@
+module.exports = {
+    'Number of pics consistent' : function(browser) {
+        browser
+            .url('http://127.0.0.1:8000/')
+            .saveScreenshot('tests_output/initial.png');
+
+        const imgGrid = "#image-grid";
+        const imgClass = '.floated_img[type="image"]';
+
+        browser.waitForElementVisible(imgGrid,'Image grid visible').assert.elementPresent("#image-grid > .floated_img:nth-of-type(4)", "Four images present before Ajax");
+        browser.waitForElementVisible(imgClass, 'Image loaded successfully')
+            .click(imgClass, function (result) {
+                this.assert.equal(true, result.status == 0, "Image clicked successfully");
+            })
+        browser.waitForElementVisible(imgGrid, 'Image grid visible after AJAX').assert.elementPresent("#image-grid > .floated_img:nth-of-type(4)", "Four images present after Ajax");
+
+    },
+
+    'Four pics returned' : function(browser) {
+        browser
+            .url('http://127.0.0.1:8000/new_images?imgset=1')
+            .saveScreenshot('tests_output/ajax.png');
+
+        const imgClass = '.floated_img[type="image"]';
+
+        browser.waitForElementVisible(imgClass, 'Image is visible').assert.elementPresent(".floated_img:nth-of-type(4)", "Four images present");
+    }
+};
